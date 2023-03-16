908963184c5f474ad59271b668b45da908be59c0
Oryam Nehoray <48219965+or-yam@users.noreply.github.com>
2023-03-16 14:58:08 +0200

    deploy test2 ▲

--- api/handler.ts
+++ api/handler.ts
@@ -1,5 +1,42 @@
+import fs from 'node:fs';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { generateSvgBadge, getWilcoStats } from '../src/index';
+
+interface Stats {
+  xp: number;
+  wilCoins: number;
+  quests: number;
+}
+
+export async function getWilcoStats(wilcoName: string): Promise<Stats> {
+  const stats = await fetch(`https://engine.wilco.gg/api/v1/profile/${wilcoName}`)
+    .then(res => res.json())
+    .then(data => ({ xp: data.xp, wilCoins: data.gems, quests: data?.finishedQuests?.length } satisfies Stats))
+    .catch(e => {
+      console.log(e.message);
+      return { xp: 0, wilCoins: 0, quests: 0 } satisfies Stats;
+    });
+  return stats;
+}
+
+function generateStatsIcon(iconName: string, value?: number): string {
+  const svgIcon = fs.readFileSync(`icons/${iconName}.svg`, 'utf-8');
+  if (!value) return svgIcon;
+  return `${svgIcon} ${value}`;
+}
+
+export function generateSvgBadge(stats: Stats) {
+  const svgLogo = generateStatsIcon('logo');
+  const svgCoin = generateStatsIcon('coin', stats.wilCoins);
+  const svgTrophy = generateStatsIcon('trophy', stats.quests);
+  const svgXp = generateStatsIcon('xp', stats.xp);
+  return `
+  <svg>
+  ${svgLogo}
+  ${svgCoin}
+  ${svgXp}
+  ${svgTrophy}
+  </svg>`;
+}
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
   const { wilconame: wilcoName } = request.query;
