dc38f03a39723ee00d1ea63c3eeda0650dc7956b
Oryam Nehoray <48219965+or-yam@users.noreply.github.com>
2023-03-20 19:26:16 +0200

    Add html

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
 //! THIS FILE NOT REALLY MEAN SOMETHING THE API IS WHAT DO THE JOB
-import fs from 'node:fs';
-import express from 'express';
-
-const app = express();
-app.use(express.static('.'));
-
-const listener = app.listen(3000, function () {
-  console.log('Your app is listening on port ' + 3000);
-});
+console.log({ '🏄‍♂️': '🦦' });
