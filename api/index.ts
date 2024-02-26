d8e26b8da414b1ef9e62a6cfc2007ccec802712b
Oryam <48219965+or-yam@users.noreply.github.com>
2024-02-26 14:42:48 +0200

    Wrap badge in `a` tag

--- api/index.ts
+++ api/index.ts
@@ -14,8 +14,9 @@ export default async function handler(request: VercelRequest, response: VercelRe
   try {
     const wilcoStats = await getWilcoStats(wilcoName);
     const svgBadge = generateSvgBadge(wilcoStats);
+    const linkWrapper = `<a href="https://app.wilco.gg/portfolio/${wilcoName}" rel="nofollow"">${svgBadge}</a>`;
     response.setHeader('Content-Type', 'image/svg+xml');
-    response.send(svgBadge);
+    response.send(linkWrapper);
   } catch (error) {
     //TODO Handle error with message and an svg without stats.
     response.status(500).send('something went wrong 😱');
